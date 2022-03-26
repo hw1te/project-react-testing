2d22da124e8e9e1f301c4413aa382a63c70e731d
hw1te <gutxander@gmail.com>
2022-03-26 13:06:14 -0300

    Requisito 1 feito.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,59 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Testa o texto dos links', () => {
+  renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: /Home/i });
+  const aboutLink = screen.getByRole('link', { name: /About/i });
+  const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favoriteLink).toBeInTheDocument();
+});
+
+test('Testa redirecionamento para página na URL / ao clicar em Home', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  expect(homeLink).toBeInTheDocument();
+  userEvent.click(homeLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('Testa redirecionamento para página na URL /about ao clicar em About', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('Testa redirecionamento para página na URL /favorites ao clicar em Favorite', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  expect(favoriteLink).toBeInTheDocument();
+  userEvent.click(favoriteLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+test('Testa se URL desconhecida redireciona para Not Found', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/pagina-desconhecida');
+
+  const notFoundText = screen.getByRole('heading', { level: 2 });
+  expect(notFoundText).toBeInTheDocument();
+});
