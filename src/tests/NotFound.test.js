6728831dfa514f8145ba1693ba41a8d73c8cb878
hw1te <gutxander@gmail.com>
2022-03-26 18:21:55 -0300

    Requisito 4 feito.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se contém um heading h2 com o texto Page requested not found 😭', () => {
+  renderWithRouter(<NotFound />);
+
+  const notFoundMessage = screen.getByRole('heading',
+    { name: /Page requested not found/i, level: 2 });
+  expect(notFoundMessage).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem com a src correta', () => {
+  renderWithRouter(<NotFound />);
+
+  const image = screen.getByAltText(/Pikachu crying because the/i);
+  expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
